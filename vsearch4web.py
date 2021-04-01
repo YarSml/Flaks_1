e4227db70dae24f80e91c257263013ac6b1b6145
YarSml <manowa.87.03.20@gmail.com>
2021-04-01 18:21:17 +0300

    первый комит

---
+++ vsearch4web.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, escape
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    '''Функция открывает файл и добавляет в него данные.'''
+    with open('vsearch.log', 'a', encoding="utf-8") as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    '''Создает страницу с результатом и выводит его'''
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    '''Создает главную страни для ввода'''
+    return render_template('entry.html',
+                           the_title='Welcome to search4letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    '''Вывод в браузере данных в лог файле'''
+    with open('vsearch.log') as log:
+        contents = []
+        for line in log:
+            contents.append([])
+            for i in line.split('|'):
+                contents[-1].append(escape(i))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title = 'View Log',
+                           the_row_titles = titles,
+                           the_data = contents)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
