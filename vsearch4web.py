78db4c852eee2b7a35d9ae9007708fc6184ebf65
YarSml <manowa.87.03.20@gmail.com>
2021-04-12 17:48:22 +0300

    11глава

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, escape, session
 from vsearch import search4letters
+from flask import copy_current_request_context
+
 from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+
+from threading import Thread
+
 
 app = Flask(__name__)
 app.config['dbconfig'] = {'host': '127.0.0.1', 'user': 'root', 'password': '12345',
@@ -15,31 +20,32 @@ def do_login() -> str:
 
 
 @app.route('/logout')
-def do_logout() ->str:
+def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out.'
 
 
-def log_request(req: 'flask_request', res: str) -> None:
-    '''Функция открывает файл и добавляет в него данные.'''
-    # with open('vsearch.log', 'a', encoding="utf-8") as log:
-    #     print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log (phrase, letters, ip, browser_string, results)
-         values (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr,
-                              req.user_agent.browser, res,))
-
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
-    '''Создает страницу с результатом и выводит его'''
+    """Создает страницу с результатом и выводит его"""
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        """Функция открывает файл и добавляет в него данные."""
+
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log (phrase, letters, ip, browser_string, results)
+             values (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr,
+                                  req.user_agent.browser, res,))
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print('***** Что-то пошло не так: ', str(err))
     return render_template('results.html',
@@ -52,7 +58,7 @@ def do_search() -> 'html':
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    '''Создает главную страни для ввода'''
+    """Создает главную страни для ввода"""
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
 
@@ -60,7 +66,7 @@ def entry_page() -> 'html':
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
-    '''Вывод в браузере данных в лог файле'''
+    """Вывод в браузере данных в лог файле"""
     try:
         with UseDatabase(app.config['dbconfig']) as cursor:
             _SQL = """select phrase, letters, ip, browser_string,
@@ -69,9 +75,9 @@ def view_the_log() -> 'html':
             contents = cursor.fetchall()
         titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
         return render_template('viewlog.html',
-                           the_title='View Log',
-                           the_row_titles=titles,
-                           the_data=contents)
+                           the_title = 'View Log',
+                           the_row_titles = titles,
+                           the_data = contents)
     except ConnectionError as err:
         print('Is your database switched on? Error:', str(err))
     except CredentialsError as err:
